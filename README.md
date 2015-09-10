b4a46d5129242cff45f47ee69ba19d284b1482a5
Oreqizer <oreqizer@gmail.com>
2015-09-10 15:52:14 +0200

    resolved one stupid bug

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 Simple TODO
 ===========
 
-RESTful API that I'm building for my portfolio.
+RESTful API that I'm building for myself just for practice.
+
+Also works as a gulp/folder structure boilerplate.
--- app/controllers/users.ctrl.js
+++ app/controllers/users.ctrl.js
@@ -1,57 +1,53 @@
 'use strict';
 
 // Get the 'users' collection
-let db = require('../../config/monk'),
+let db = require('../../config/monk')(),
     users = db.get('users');
 
 // Error handler controller
 //let errors = require('./errors.ctrl');
-
-module.exports = function() {
   
-  // API endpoints:
-  // Returns all users
-  function findAll(req, res) {
+// API endpoints:
+// Returns all users
+exports.findAll = function(req, res) {
     
-    users.find({})
-    .then(data => {
-      res.json(data);
-    })
-    .catch(err => {
+  users.find({})
+  .then(data => {
+    res.json(data);
+  })
+  .catch(err => {
 //      errors.db(err);
-    });
-    
-  }
-  
-  // Returns an user by ID
-  function find(req, res, next, id) {
-    
-    users.find(id)
-    .then(user => {
-      if (user !== {}) {
-        req.user = user;
-        next();
-      } else {
-        res.redirect('/users/notfound');
-      }
-    }, err => {
-      next(err);
-    });
-    
-  }
-  
-  // Logs in an user
-  function login(req, res) {
-    
-    
-    
-  }
-  
-  // Registers an user
-  function register(req, res) {
-    
-    
-    
-  }
-  
+  });
+
 };
+  
+// Returns an user by ID
+exports.find = function(req, res, next, id) {
+
+  users.find(id)
+  .then(user => {
+    if (user !== {}) {
+      req.user = user;
+      next();
+    } else {
+      res.redirect('/users/notfound');
+    }
+  }, err => {
+    next(err);
+  });
+
+};
+  
+// Logs in an user
+exports.login = function(req, res) {
+
+
+
+}
+  
+// Registers an user
+exports.register = function(req, res) {
+
+
+
+}
